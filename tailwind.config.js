eabeef8237a5d18bacce2e6e69df1c946de50660
Ali Saffari <parvazled@gmail.com>
2021-09-27 15:29:24 +0330

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    fontFamily: {
+        sans: ['Red Hat Display', 'sans-serif']
+    },
+    theme: {
+        colors: {
+            'paleBlue': '#e0e8ff',
+            'brightBlue': '#3829e0',
+            'veryPaleBlue': '#f5f7ff',
+            'desaturatedBlue': '#7280a7',
+            'darkBlue': '#1f2f56',
+            'white': '#ffffff'
+        },
+        extend: {
+            backgroundImage: {
+                'mobile-background': "url('/images/pattern-background-mobile.svg')",
+                'desktop-background': "url('/images/pattern-background-desktop.svg')",
+            }
+        }
+    }
+}
